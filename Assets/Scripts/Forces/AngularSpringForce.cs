8ac8573a3969faa622b2b09e4d22743b83a2bd66
Thom Hurks <thomhurks@gmail.com>
2016-06-23 22:57:33 +0200

    Stub for angular spring force

---
+++ Assets/Scripts/Forces/AngularSpringForce.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class AngularSpringForce : Force
+{
+    public void ApplyForce(ParticleSystem a_ParticleSystem)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Draw()
+    {
+        throw new System.NotImplementedException();
+    }
+}
