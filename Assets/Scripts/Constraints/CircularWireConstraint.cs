d7e0a75c4e7cc0db5c026f38fe0b70728a3d928a
Thom Hurks <thomhurks@gmail.com>
2016-06-23 13:32:22 +0200

    Whitespace and added draw function

--- Assets/Scripts/Constraints/CircularWireConstraint.cs
+++ Assets/Scripts/Constraints/CircularWireConstraint.cs
@@ -3,49 +3,63 @@ using System.Collections;
 
 public class CircularWireConstraint : Constraint
 {
-	private BlockSparseMatrix.MatrixBlock m_MatrixBlockJ;
-	private BlockSparseMatrix.MatrixBlock m_MatrixBlockJDot;
-	private Particle m_Particle;
-	private Vector2 m_Center;
-	private float m_RadiusSquared;
+    private BlockSparseMatrix.MatrixBlock m_MatrixBlockJ;
+    private BlockSparseMatrix.MatrixBlock m_MatrixBlockJDot;
+    private Particle m_Particle;
+    private Vector2 m_Center;
+    private float m_Radius;
+    private float m_RadiusSquared;
 
-	public CircularWireConstraint(Particle a_Particle, Vector2 a_Center, float a_Radius, ParticleSystem a_System)
-	{
-		m_Particle = a_Particle;
-		m_Center = a_Center;
-		m_RadiusSquared = a_Radius * a_Radius;
-		int j = a_System.GetParticleIndex(a_Particle) * a_System.GetParticleDimension();
-		int i = a_System.AddConstraint(this);
-		int iLength = GetConstraintDimension();
-		int jLength = a_System.GetParticleDimension();
-		m_MatrixBlockJ = a_System.MatrixJ.CreateMatrixBlock(i, j, iLength, jLength);
-		m_MatrixBlockJDot = a_System.MatrixJDot.CreateMatrixBlock(i, j, iLength, jLength);
-	}
+    public CircularWireConstraint(Particle a_Particle, Vector2 a_Center, float a_Radius, ParticleSystem a_System)
+    {
+        m_Particle = a_Particle;
+        m_Center = a_Center;
+        m_Radius = a_Radius;
+        m_RadiusSquared = a_Radius * a_Radius;
+        int j = a_System.GetParticleIndex(a_Particle) * a_System.GetParticleDimension();
+        int i = a_System.AddConstraint(this);
+        int iLength = GetConstraintDimension();
+        int jLength = a_System.GetParticleDimension();
+        m_MatrixBlockJ = a_System.MatrixJ.CreateMatrixBlock(i, j, iLength, jLength);
+        m_MatrixBlockJDot = a_System.MatrixJDot.CreateMatrixBlock(i, j, iLength, jLength);
+    }
 
-	public void UpdateJacobians(ParticleSystem a_ParticleSystem)
-	{
-		Vector2 relative = m_Particle.Position - m_Center;
-		m_MatrixBlockJ.data[0] = relative.x;
-		m_MatrixBlockJ.data[1] = relative.y;
-		m_MatrixBlockJDot.data[0] = m_Particle.Velocity.x;
-		m_MatrixBlockJDot.data[1] = m_Particle.Velocity.y; // TODO: VERIFY THIS
-	}
+    public void UpdateJacobians(ParticleSystem a_ParticleSystem)
+    {
+        Vector2 relative = m_Particle.Position - m_Center;
+        m_MatrixBlockJ.data[0] = relative.x;
+        m_MatrixBlockJ.data[1] = relative.y;
+        m_MatrixBlockJDot.data[0] = m_Particle.Velocity.x;
+        m_MatrixBlockJDot.data[1] = m_Particle.Velocity.y; // TODO: VERIFY THIS
+    }
 
-	public float GetValue(ParticleSystem a_ParticleSystem)
-	{
-		Vector2 relative = m_Particle.Position - m_Center;
-		return (relative.sqrMagnitude - m_RadiusSquared) / 2;
-	}
+    public float GetValue(ParticleSystem a_ParticleSystem)
+    {
+        Vector2 relative = m_Particle.Position - m_Center;
+        return (relative.sqrMagnitude - m_RadiusSquared) / 2;
+    }
 
-	public float GetDerivativeValue(ParticleSystem a_ParticleSystem)
-	{
-		Vector2 relative = m_Particle.Position - m_Center;
-		return Vector2.Dot(m_Particle.Velocity, relative);
-	}
+    public float GetDerivativeValue(ParticleSystem a_ParticleSystem)
+    {
+        Vector2 relative = m_Particle.Position - m_Center;
+        return Vector2.Dot(m_Particle.Velocity, relative);
+    }
 
-	public int GetConstraintDimension()
-	{
-		return 1;
-	}
+    public int GetConstraintDimension()
+    {
+        return 1;
+    }
+
+    public void Draw()
+    {
+        GL.Begin(GL.LINES);
+        GL.Color(Color.green);
+        for (int i = 0; i < 360; i = i + 18)
+        {
+            float degInRad = i * Mathf.PI / 180;
+            GL.Vertex(new Vector2(m_Center.x + Mathf.Cos(degInRad) * m_Radius, m_Center.y + Mathf.Sin(degInRad) * m_Radius));
+        }
+        GL.End();
+    }
 
 }
