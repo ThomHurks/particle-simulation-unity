a5837ce0e7ab802742abeb588b2e1d8535ceae39
Thom Hurks <thomhurks@gmail.com>
2016-07-07 10:49:08 +0200

    Added new solver and scenarios to UI. Added stub scenario.

---
+++ Assets/Scripts/Scenarios/TrainScenario.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainScenario : Scenario
+{
+    public void CreateScenario(ParticleSystem a_ParticleSystem)
+    {
+        throw new System.NotImplementedException();
+    }
+}
