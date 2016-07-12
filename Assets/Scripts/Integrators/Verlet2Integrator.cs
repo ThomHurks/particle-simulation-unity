31bd075f5c5ac031c1858ba060a528bbd748037f
LightChaosman <lightchaosman@hotmail.com>
2016-07-12 22:50:44 +0200

    Reworked Verlet2, now its readable

--- Assets/Scripts/Integrators/Verlet2Integrator.cs
+++ Assets/Scripts/Integrators/Verlet2Integrator.cs
@@ -14,7 +14,52 @@
         originals = new double[particleDimensions];
     }
 
+
     public override void Step(ParticleSystem a_ParticleSystem, float a_DeltaTime)
+    {
+        int n = a_ParticleSystem.ParticleDimensions / 4;
+        float dt = a_DeltaTime;
+        a_ParticleSystem.ParticlesGetState(originals); //backup original locations and speeds
+
+        a_ParticleSystem.ParticleDerivative(temp1);
+        for (int i = 0; i < n; i++)
+        {
+            int i4 = i * 4;
+            //+0 to make it more readable, compiler will fix it :)
+            temp2[i4 + 0] = originals[i4 + 0] + temp1[i4 + 0] * dt / 2; // x_{i+1} = x_i + v_i dt
+            temp2[i4 + 1] = originals[i4 + 1] + temp1[i4 + 1] * dt / 2;
+            temp2[i4 + 2] = originals[i4 + 2] + temp1[i4 + 2] * dt / 2;
+            temp2[i4 + 3] = originals[i4 + 3] + temp1[i4 + 3] * dt / 2;
+        }
+        a_ParticleSystem.ParticlesSetState(temp2); //Did half an euler step
+
+      
+        a_ParticleSystem.ParticleDerivative(temp1); 
+        for (int i = 0; i < n; i++)
+        {
+            int i4 = i * 4;
+            //+0 to make it more readable, compiler will fix it :)
+            temp2[i4 + 0] = originals[i4 + 0] + temp1[i4 + 0] * dt; // x_{i+1} = x_i + v_i dt
+            temp2[i4 + 1] = originals[i4 + 1] + temp1[i4 + 1] * dt;
+            temp2[i4 + 2] = originals[i4 + 2] + temp1[i4 + 2] * dt;
+            temp2[i4 + 3] = originals[i4 + 3] + temp1[i4 + 3] * dt;
+        }
+        a_ParticleSystem.ParticlesSetState(temp2); //Did a full midpoint step
+
+
+        a_ParticleSystem.ParticleDerivative(temp1); 
+        for (int i = 0; i < n; i++)
+        {
+            int i4 = i * 4;
+            temp2[i4 + 2] = originals[i4 + 2] + temp1[i4 + 2] * a_DeltaTime / 2f;
+            temp2[i4 + 3] = originals[i4 + 3] + temp1[i4 + 3] * a_DeltaTime / 2f;
+        }
+        a_ParticleSystem.ParticlesSetState(temp2);
+    }
+
+
+
+    /* public override void Step(ParticleSystem a_ParticleSystem, float a_DeltaTime)
     {
         a_ParticleSystem.ParticlesGetState(originals); //backup original locations and speeds
 
@@ -34,10 +79,9 @@
         int n = a_ParticleSystem.ParticleDimensions / 4;
         for (int i = 0; i < n; i++)
         {
-            int i4 = i * 4;
-            temp2[i * 4 + 2] = originals[i4 + 2] + temp3[i4 + 2] * a_DeltaTime / 2f;
-            temp2[i * 4 + 3] = originals[i4 + 3] + temp3[i4 + 3] * a_DeltaTime / 2f;
+            temp2[i * 4 + 2] = temp1[i * 4] + temp3[i * 4 + 2] * a_DeltaTime / 2f;
+            temp2[i * 4 + 3] = temp1[i * 4] + temp3[i * 4 + 3] * a_DeltaTime / 2f;
         }
         a_ParticleSystem.ParticlesSetState(temp2);
-    }
+    }*/
 }
