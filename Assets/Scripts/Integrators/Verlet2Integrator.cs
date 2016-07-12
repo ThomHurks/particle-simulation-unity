2376faa1ef53614a275aa3e1acc8348f1911168f
LightChaosman <lightchaosman@hotmail.com>
2016-07-13 00:33:11 +0200

    minor bugfix

--- Assets/Scripts/Integrators/Verlet2Integrator.cs
+++ Assets/Scripts/Integrators/Verlet2Integrator.cs
@@ -1,16 +1,18 @@
 ﻿public class Verlet2Integrator : Integrator
 {
-    private double[] temp1;
-    private double[] temp2;
-    private double[] temp3;
+    private double[] derivativeI;
+    private double[] stateStorage;
+    private double[] derivativeIPlusOneHalve;
+    private double[] derivativeIPlusOneFourth;
     private double[] originals;
 
     public override void Initialize(ParticleSystem a_ParticleSystem)
     {
         int particleDimensions = a_ParticleSystem.ParticleDimensions; // = 4n
-        temp1 = new double[particleDimensions];
-        temp2 = new double[particleDimensions];
-        temp3 = new double[particleDimensions];
+        stateStorage = new double[particleDimensions];
+        derivativeI = new double[particleDimensions];
+        derivativeIPlusOneHalve = new double[particleDimensions];
+        derivativeIPlusOneFourth = new double[particleDimensions];
         originals = new double[particleDimensions];
     }
 
@@ -21,38 +23,38 @@
         float dt = a_DeltaTime;
         a_ParticleSystem.ParticlesGetState(originals); //backup original locations and speeds
 
-        a_ParticleSystem.ParticleDerivative(temp1); // v,a
+        a_ParticleSystem.ParticleDerivative(derivativeI); // v,a
         for (int i = 0; i < n; i++)
         { 
             int i4 = i * 4;
             //+0 to make it more readable, compiler will fix it :)
-            temp2[i4 + 0] = originals[i4 + 0] + temp1[i4 + 0] * dt / 4; // x_{i+1} = x_i + v_i dt + a_i dtsq
-            temp2[i4 + 1] = originals[i4 + 1] + temp1[i4 + 1] * dt / 4;
-            temp2[i4 + 2] = originals[i4 + 2] + temp1[i4 + 2] * dt / 4;
-            temp2[i4 + 3] = originals[i4 + 3] + temp1[i4 + 0] * dt / 4;
+            stateStorage[i4 + 0] = originals[i4 + 0] + derivativeI[i4 + 0] * dt / 4; // x_{i+1} = x_i + v_i dt + a_i dtsq
+            stateStorage[i4 + 1] = originals[i4 + 1] + derivativeI[i4 + 1] * dt / 4;
+            stateStorage[i4 + 2] = originals[i4 + 2] + derivativeI[i4 + 2] * dt / 4;
+            stateStorage[i4 + 3] = originals[i4 + 3] + derivativeI[i4 + 0] * dt / 4;
         }
-        a_ParticleSystem.ParticlesSetState(temp2); // 1/4 euler step
+        a_ParticleSystem.ParticlesSetState(stateStorage); // 1/4 euler step
 
-        a_ParticleSystem.ParticleDerivative(temp1); // v,a
+        a_ParticleSystem.ParticleDerivative(derivativeIPlusOneFourth); // v,a
         for (int i = 0; i < n; i++)
         { 
             int i4 = i * 4;
             //+0 to make it more readable, compiler will fix it :)
-            temp2[i4 + 0] = originals[i4 + 0] + originals[i4 + 2] * dt + temp1[i4 + 2] * dt * dt / 2f; // x_{i+1} = x_i + v_i dt + a_i dtsq
-            temp2[i4 + 1] = originals[i4 + 1] + originals[i4 + 3] * dt + temp1[i4 + 3] * dt * dt / 2f;
-            temp2[i4 + 2] = originals[i4 + 2];
-            temp2[i4 + 3] = originals[i4 + 3];
+            stateStorage[i4 + 0] = originals[i4 + 0] + originals[i4 + 2] * dt + derivativeIPlusOneFourth[i4 + 2] * dt * dt / 2f; // x_{i+1} = x_i + v_i dt + a_i dtsq
+            stateStorage[i4 + 1] = originals[i4 + 1] + originals[i4 + 3] * dt + derivativeIPlusOneFourth[i4 + 3] * dt * dt / 2f;
+            stateStorage[i4 + 2] = originals[i4 + 2];
+            stateStorage[i4 + 3] = originals[i4 + 3];
         }
-        a_ParticleSystem.ParticlesSetState(temp2); // same first step as normal verlet, except this time based on midpoint
+        a_ParticleSystem.ParticlesSetState(stateStorage); // same first step as normal verlet, except this time based on midpoint
 
-        a_ParticleSystem.ParticleDerivative(temp3); // v+1,a+1
+        a_ParticleSystem.ParticleDerivative(derivativeIPlusOneHalve); // v+1,a+1
         for (int i = 0; i < n; i++)
         {
             int i4 = i * 4;
-            temp2[i4 + 2] = originals[i4 + 2] + (temp3[i4 + 2] + temp1[i4 + 2]) * a_DeltaTime / 2f;
-            temp2[i4 + 3] = originals[i4 + 3] + (temp3[i4 + 3] + temp1[i4 + 3]) * a_DeltaTime / 2f;
+            stateStorage[i4 + 2] = originals[i4 + 2] + (derivativeIPlusOneHalve[i4 + 2] + derivativeI[i4 + 2]) * a_DeltaTime / 2f;
+            stateStorage[i4 + 3] = originals[i4 + 3] + (derivativeIPlusOneHalve[i4 + 3] + derivativeI[i4 + 3]) * a_DeltaTime / 2f;
         }
-        a_ParticleSystem.ParticlesSetState(temp2);
+        a_ParticleSystem.ParticlesSetState(stateStorage);
     }
 
 
