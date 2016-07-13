ef02ec9800bcca45d0a64306ca6ce03338a87b6f
LightChaosman <lightchaosman@hotmail.com>
2016-07-13 02:40:38 +0200

    Relocated speed test particle to top of ellipse

--- Assets/Scripts/Scenarios/SpeedTestScenario.cs
+++ Assets/Scripts/Scenarios/SpeedTestScenario.cs
@@ -8,9 +8,9 @@ public class SpeedTestScenario : Scenario
     {
 
         Particle prev = new Particle(1f);
-        prev.Position = new Vector2(-2, 0);
+        prev.Position = new Vector2(0, 1);
         a_ParticleSystem.AddParticle(prev);
-        prev.Velocity = Vector2.up * 40;
+        prev.Velocity = Vector2.right * 40;
         new EllipticalWireConstraint(prev, Vector2.left * Mathf.Sqrt(3), Vector2.right * Mathf.Sqrt(3), a_ParticleSystem); // 2x4 ellipse
         //a_ParticleSystem.AddForce(new ViscousDragForce(0.01f));
        
